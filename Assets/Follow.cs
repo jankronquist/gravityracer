c82f32d8b9ea16a13e4a97e1987b6223de8f67e0
Jan Kronquist <jan.kronquist@gmail.com>
2021-01-24 09:37:27 +0100

    Flying square

---
+++ Assets/Follow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Rigidbody2D target;
+
+    void FixedUpdate() {
+        Vector2 t = Vector2.Lerp(this.transform.position, target.position + 1.4f * target.velocity, Time.deltaTime);
+        this.transform.position = new Vector3(t.x, t.y, this.transform.position.z);
+    }
+}
