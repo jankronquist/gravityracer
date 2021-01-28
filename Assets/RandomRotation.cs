5320e25c73c14c5fc21e0daeeae918177c0af6c3
Jan Kronquist <jan.kronquist@gmail.com>
2021-01-28 21:10:22 +0100

    Added intro screen

---
+++ Assets/RandomRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    private Quaternion originalRotation;
+    private float currentZ;
+    private float originalZ;
+    private float targetZ;
+    private float nextSwitch;
+    public float delay;
+    public float rotationSize;
+
+    void Start()
+    {
+        originalRotation = this.transform.rotation;
+        originalZ = this.transform.rotation.eulerAngles.z;
+        currentZ = originalZ;
+        targetZ = originalZ;
+        nextSwitch = Time.time + delay;
+    }
+
+    void FixedUpdate() {
+        if (Time.time > nextSwitch) {
+            targetZ = originalZ + Random.Range(-rotationSize, rotationSize);
+            nextSwitch = Time.time + delay + (Random.value * delay);
+        }
+        currentZ = Mathf.LerpAngle(currentZ, targetZ, 0.05f);
+        this.transform.rotation = Quaternion.Euler(0, 0, currentZ);
+    }
+}
